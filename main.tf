8b38cab6588320ac9366dbfe6b368662803e173a
Taehyun Kim <thkim0022@icloud.com>
2022-12-27 17:37:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_kms_key" "this" {
    description             = "Encryption Key for the S3 Bucket"
-   deletion_window_in_days = var.kms_deletion_window_in_days
+   deletion_window_in_days = var.deletion_window_in_days
 
    tags = merge(
      var.tags,
      {
-       "Name": "kms-${var.kms_alias_name}",
+       "Name": "kms-${var.alias_name}",
        "Type": "kms"
      }
    )
  }
 
  resource "aws_kms_alias" "this" {
-   name          = "alias/${lower(var.kms_alias_name)}"
+   name          = "alias/${lower(var.alias_name)}"
    target_key_id = aws_kms_key.this.key_id
 
    depends_on = [
